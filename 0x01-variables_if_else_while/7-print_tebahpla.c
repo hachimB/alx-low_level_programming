71c3c7011950028650c2736c741f9010ace10063
hachimB <boubacarhachim@gmail.com>
2023-06-18 15:30:35 +0100

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char as = 122;
+while (as > 96)
+{
+putchar (as);
+as--;
+}
+putchar ('\n');
+return (0);
+}
