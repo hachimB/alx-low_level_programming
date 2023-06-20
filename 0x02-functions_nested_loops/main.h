056bceb2786ddb342c413d1cfb4a5760cadd8b75
hachimB <boubacarhachim@gmail.com>
2023-06-20 11:37:19 +0100

    header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+void put(void);
+void put(void)
+{
+putchar ('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
+}
