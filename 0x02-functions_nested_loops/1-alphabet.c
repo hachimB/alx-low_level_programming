4862e5d84d013e5c3de358cfac25c8cbb0668334
hachimB <boubacarhachim@gmail.com>
2023-06-20 12:52:22 +0100

    the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+char x = 97;
+while (x < 123)
+{
+_putchar (x);
+x++;
+}
+_putchar ('\n');
 }
