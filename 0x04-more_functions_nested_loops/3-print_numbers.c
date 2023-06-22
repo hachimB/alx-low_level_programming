b572b137dcbe9ded03e2e1af9d1e562de1d81472
hachimB <boubacarhachim@gmail.com>
2023-06-22 19:50:58 +0100

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Return: nothing.
+ */
+void print_numbers(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+_putchar (x);
+}
+_putchar ('\n');
+}
