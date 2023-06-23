835d4cc92a640f8013514ec18b7835621d9cadbf
hachimB <boubacarhachim@gmail.com>
2023-06-23 09:09:34 +0100

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - contains the code
+ *
+ *@a : interger.
+ *
+ * Return: nothing.
+ */
+void print_diagonal(int a)
+{
+int x;
+int y;
+if (a > 0)
+{
+for (x = 1; x <= a; x++)
+{
+for (y = 1; y <= x; y++)
+{
+_putchar (32);
+}
+_putchar (92);
+_putchar ('\n');
+}
+}
+else
+_putchar ('\n');
+}
