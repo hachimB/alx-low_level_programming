431a80f7ceb62c85156b9e3a6fef1410f471d207
hachimB <boubacarhachim@gmail.com>
2023-06-25 01:18:31 +0100

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,12 +10,11 @@ void print_triangle(int size)
 {
 int x;
 int y;
-int a;
 if (size >= 1)
 {
 for (x = 1; x <= size; x++)
 {
-for (a = size - x; a >= 1; a--)
+for (y = x; y < size; y++)
 {
 _putchar (' ');
 }
