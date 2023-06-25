d5b6207fe2bf565c49905479f9b7b7033d526659
hachimB <boubacarhachim@gmail.com>
2023-06-25 20:30:01 +0100

    print numbers with _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - contains the code
+ *
+ *@n:integer.
+ *
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+int x;
+if (n < 0)
+{
+x = -n;
+_putchar ('-');
+}
+else
+x = n;
+if (x / 10)
+{
+print_number(x / 10);
+}
+_putchar ((x % 10) + '0');
+}
