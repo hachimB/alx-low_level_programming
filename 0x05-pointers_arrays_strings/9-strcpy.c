1367907216eeabc2056e687c60cda9f7e05b5626
hachimB <boubacarhachim@gmail.com>
2023-06-27 16:45:16 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - contains the code
+ *
+ *@dest : string.
+ *
+ *@src : string.
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+int j;
+char x;
+while (src[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < i; j++)
+{
+x = src[j];
+dest[j] = x;
+}
+return (dest);
+}
