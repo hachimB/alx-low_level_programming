27c8e9a2946f814052a0a2f4d06ca8e7f26248c9
hachimB <boubacarhachim@gmail.com>
2023-07-05 07:52:26 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-char *p;
-*s = *p;
-_putchar (p);
-_putchar ('\n');
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s); 
+_puts_recursion(s + 1);
 }
