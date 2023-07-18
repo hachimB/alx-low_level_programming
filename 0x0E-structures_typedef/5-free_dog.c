339b543487c0f4a767c73317cd8b7f4450d8ebd6
hachimB <boubacarhachim@gmail.com>
2023-07-18 09:42:11 +0100

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - contains the code
+ * @d : pointer of type dog_t.
+ * Return: nothing.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
