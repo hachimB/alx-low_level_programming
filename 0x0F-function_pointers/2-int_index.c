0e8d8cbe082421327f3ffaaf625fd03e980fe62d
hachimB <boubacarhachim@gmail.com>
2023-07-19 19:09:47 +0100

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - contains the code.
+ * @size: the number of elements in the array (array).
+ * @array: array of integers.
+ * @cmp: function pointer.
+ * Return: nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (i);
+}
