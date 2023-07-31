be8160fda670e257aee863c33febd3b4a251d381
hachimB <boubacarhachim@gmail.com>
2023-07-31 18:22:23 +0100

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,17 +5,18 @@
 
 /**
  * free_listint2 - frees a listint_t list.
- * @head : double pointer to the first node.
+ * @head : double pointer.
  * Return: nothing.
  */
 void free_listint2(listint_t **head)
 {
-listint_t *ptr;
-while (*head)
+listint_t *ptr, *t;
+ptr = *head;
+while (ptr)
 {
-ptr = (*head)->next;
-free(*head);
-*head = ptr;
+t = ptr->next;
+free(ptr);
+ptr = t;
 }
 *head = NULL;
 }
