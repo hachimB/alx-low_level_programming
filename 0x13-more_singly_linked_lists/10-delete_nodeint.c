87f200b5a332ea7d64cae98f35217a9c7a1cf64f
hachimB <boubacarhachim@gmail.com>
2023-08-01 17:56:44 +0100

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *previous, *current;
 previous = *head;
 current = *head;
-if (*head == NULL || previous == NULL || current == NULL)
+if (*head == NULL)
 return (-1);
 else if (index == 0) /* We will delete the first node of the list */
 {
@@ -24,7 +24,7 @@ current = NULL;
 }
 else
 {
-while (index != 0)
+while (index != 0 && previous != NULL && current != NULL)
 {
 previous = current;
 current = current->next;
