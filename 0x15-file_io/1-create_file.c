e96725a61564d714589f5f154fabe5a510164a22
hachimB <boubacarhachim@gmail.com>
2023-08-07 23:23:20 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,13 @@ if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-while (text_content[i])
+for (i = 0; text_content[i];)
 i++;
 }
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-write(fd, text_content, i);
+wr = write(fd, text_content, i);
 if (wr == -1)
 return (-1);
 close(fd);
