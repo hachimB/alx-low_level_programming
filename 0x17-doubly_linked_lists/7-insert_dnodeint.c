1c9d54feaad58edf26040069d71e1b2e85bf4116
hachimB <boubacarhachim@gmail.com>
2023-09-15 18:07:48 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,33 @@
 #include "lists.h"
-
+/**
+ * create_new_node - creates a new node.
+ * @n: data to insert in the list.
+ * Return: the new node.
+ */
+dlistint_t *create_new_node(int n)
+{
+dlistint_t *temp = malloc(sizeof(dlistint_t));
+if (temp == NULL)
+return (NULL);
+temp->prev = NULL;
+temp->n = n;
+temp->next = NULL;
+return (temp);
+}
+/**
+ * insert_between - inserts a new node.
+ * @ptr: ptr pointer.
+ * @ptr2: ptr2 pointer.
+ * @temp : temp pointer.
+ * Return: nothing.
+ */
+void insert_between(dlistint_t *ptr, dlistint_t *ptr2, dlistint_t *temp)
+{
+temp->prev = ptr;
+temp->next = ptr2;
+ptr->next = temp;
+ptr2->prev = temp;
+}
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: head pointer.
@@ -12,12 +40,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 unsigned int count = 0;
 dlistint_t *ptr;
 dlistint_t *ptr2 = NULL;
-dlistint_t *temp = malloc(sizeof(dlistint_t));
+dlistint_t *temp = create_new_node(n);
 if (temp == NULL)
 return (NULL);
-temp->prev = NULL;
-temp->n = n;
-temp->next = NULL;
 if (*h == NULL)
 {
 free(temp);
@@ -38,7 +63,7 @@ ptr = add_dnodeint(h, n);
 free(temp);
 return (ptr);
 }
-while (idx > 1)
+while (idx > 0)
 {
 ptr = ptr->next;
 idx--;
@@ -48,13 +73,7 @@ if (ptr->next == NULL)
 ptr = add_dnodeint_end(h, n);
 return (ptr);
 }
-else
-{
 ptr2 = ptr->next;
-ptr->next = temp;
-ptr2->prev = temp;
-temp->prev = ptr;
-temp->next = ptr2;
-}
+insert_between(ptr, ptr2, temp);
 return (ptr);
 }
