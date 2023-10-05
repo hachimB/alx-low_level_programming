1f56e07ff353e4d75a02aae32e5e479a2f0db5bd
hachimB <boubacarhachim@gmail.com>
2023-10-06 00:37:20 +0100

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ hash_node_t *ptr = NULL;
 char *key_cp;
 char *val_cp;
 hash_node_t *new = malloc(sizeof(hash_node_t));
-if (new == NULL || key == NULL || ht == NULL || value == NULL)
+if (new == NULL || key == NULL || ht == NULL)
 return (0);
 index = key_index((const unsigned char *)key, ht->size);
 key_cp = strdup(key);
@@ -29,7 +29,7 @@ else
 {
 ptr = ht->array[index];
 new->next = ptr;
-ht->array[index] = new;
+ptr = new;
 }
 return (1);
 }
