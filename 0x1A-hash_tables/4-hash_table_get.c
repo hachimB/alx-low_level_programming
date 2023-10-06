4a3ac241dded77dea5c8a4b06d4ce36da5e5b412
hachimB <boubacarhachim@gmail.com>
2023-10-06 08:48:00 +0100

    >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,11 +8,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int index;
+hash_node_t *ptr;
 if (ht == NULL || key == NULL)
 return (NULL);
 index = key_index((const unsigned char *)key, ht->size);
+ptr = ht->array[index];
+while (ptr)
+{
 if (ht->array[index] == NULL || strcmp(ht->array[index]->key, key) != 0)
 return (NULL);
-else
+if (strcmp(ht->array[index]->key, key) == 0)
 return (ht->array[index]->value);
+ptr = ptr->next;
+}
+return (NULL);
 }
