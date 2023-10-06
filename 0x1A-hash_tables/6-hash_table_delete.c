9e07a6165ef3f45027a4d4f7ffd4ffed60f3a37e
hachimB <boubacarhachim@gmail.com>
2023-10-06 10:43:04 +0100

    >>> print(ht)

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht:the hash table
+ * Return: nothing.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int index;
+hash_node_t *ptr;
+hash_node_t *temp;
+if (ht == NULL)
+return;
+for (index = 0; index < ht->size; index++)
+{
+ptr = ht->array[index];
+while (ptr)
+{
+temp = ptr;
+ptr = ptr->next;
+free(temp);
+temp = NULL;
+}
+}
+free(ht->array);
+free(ht);
+}
