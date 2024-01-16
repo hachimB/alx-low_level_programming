aa59ef8493720ff7c7bb775a9a32daa3bf1a68d3
hachimB <boubacarhachim@gmail.com>
2024-01-16 01:21:11 +0100

    ok

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,22 @@
 int linear_search(int *array, size_t size, int value)
 {
 size_t i;
+size_t index = 0;
 if (!array)
 return (-1);
+else
+{
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 if (array[i] == value)
-return (i);
+{
+index = i;
+break;
 }
-return (-1);
+}
+if (index == 0)
+index = -1;
+}
+return (index);
 }
